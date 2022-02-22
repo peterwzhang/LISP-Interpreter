bdbac1ccba78c37833c8a953ebc6bc70f8be94b9
Peter Zhang <peter.w.zhang@gmail.com>
2022-02-22 02:49:18 -0600

    cleaning, added makefile

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+#include "../include/fmt/format.h"
+#include "../include/parser.h"
+
+int main() {
+    fmt::print(">>>");
+    std::string input;
+    std::cin >> input;
+    Scanner scanner(input);
+    return 0;
+}
--- src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "../include/fmt/format.h"
-
-int main(){
-    fmt::print("Hello, {}!\n", "world");
-    return 0;
-}
