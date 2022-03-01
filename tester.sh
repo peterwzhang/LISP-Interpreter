0d6c1cee2d77901526a2a2da489ceb8f222e9707
Peter Zhang <peter.w.zhang@gmail.com>
2022-03-01 17:02:12 -0600

    update test files to be lisp instead of txt

--- tester.sh
+++ tester.sh
@@ -10,8 +10,8 @@ getInput() {
 
 runTest() {
     echo "Running test case $1"
-    ./tester/kamincpp.out < tester/tests/test"$1".txt > tester/tests/test"$1".out
-    python3 src/project1.py < tester/tests/test"$1".txt > tester/tests/test"$1".ans
+    ./tester/kamincpp.out < tester/tests/test"$1".lisp > tester/tests/test"$1".out
+    python3 src/project1.py < tester/tests/test"$1".lisp > tester/tests/test"$1".ans
     matchedLines=$(diff -w -y --suppress-common-lines "tester/tests/test$1.ans" "tester/tests/test$1.out" | wc -l | tr -d '[:space:]')
     if [ "$matchedLines" -eq "0" ]; then
         echo "Test case $1 passed"
