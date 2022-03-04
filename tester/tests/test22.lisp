77169e459e7cd1549f1487e98a9ef028edf91efd
Peter Zhang <peter.w.zhang@gmail.com>
2022-03-03 20:45:22 -0600

    add some function tests

---
+++ tester/tests/test22.lisp
@@ -0,0 +1,13 @@
+(define caar (x) (car (car x)))
+(define cadr (x) (car (cdr x)))
+(define cdar (x) (cdr (car x)))
+(define cddr (x) (cdr (cdr x)))
+(set list '(1 2 3 4 5))
+(cadr list)
+(cddr list)
+(set list '((1 2) 3 4 5))
+(caar list)
+(cadr list)
+(cdar list)
+(cddr list)
+quit
